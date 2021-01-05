f9525737f95780459b751596d05349e113895518
Sophia Hadash <sophiahadash@gmail.com>
2021-01-05 11:34:50 +0100

    tests for thresholds.

--- test/manual/src/App.tsx
+++ test/manual/src/App.tsx
@@ -7,7 +7,7 @@ import explanation from "./explanation.json";
 function App() {
     return (
     <div className="App">
-      <FeatureListVisualizer explanation={explanation as ExplanationObject} framing={"positive"} lct={"label"} threshold={0} />
+      <FeatureListVisualizer explanation={explanation as ExplanationObject} framing={"original"} lct={"none"} thresholdBadge={0} thresholdOmit={0.007} />
     </div>
   );
 }
