e9208dc1f140976faf165807e0b44004c9e2a893
Sophia Hadash <sophiahadash@gmail.com>
2021-01-11 15:35:14 +0100

    include Toulmin in manual testing

--- test/manual/src/App.tsx
+++ test/manual/src/App.tsx
@@ -1,7 +1,7 @@
 import {ExplanationObject} from "argueview/dist/typings/IExplanation";
 import React from 'react';
 import './App.css';
-import { FeatureListVisualizer } from "argueview";
+import { FeatureListVisualizer, ToulminVisualizer } from "argueview";
 import explanation from "./explanation.json";
 import explanationNegative from "./explanation_negative.json";
 import explanationExceptions from "./explanation_exceptions.json";
@@ -9,7 +9,8 @@ import explanationExceptions from "./explanation_exceptions.json";
 function App() {
     return (
     <div className="App">
-      <FeatureListVisualizer explanation={explanationExceptions as ExplanationObject} framing={"decision-class"} lct={"none"} thresholdBadge={-1} thresholdOmit={0.001} visualization={"bar"} />
+        <ToulminVisualizer explanation={explanationExceptions as ExplanationObject} />
+        <FeatureListVisualizer explanation={explanationExceptions as ExplanationObject} framing={"decision-class"} lct={"none"} thresholdBadge={-1} thresholdOmit={0.001} visualization={"bar"} />
     </div>
   );
 }
